634ee8a6513f8624a75a69cf2b55796171be036f
虎虎 <1078013818@qq.com>
2022-12-15 13:36:10 +0800

    feat:首页结构

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,8 +1,7 @@
-import { View, Image } from '@tarojs/components'
+import { Image, Swiper, SwiperItem } from '@tarojs/components'
 import { useState } from 'react'
 import { TabItem, TabLine, TabList, TabPanel, TabPanels, Tabs } from 'src/components'
-import { Box, Flex, SearchInput, VStack } from 'src/features'
-import { useParams } from 'src/hooks'
+import { Box, Center, Flex, SearchInput, VStack } from 'src/features'
 import { postApiV1AppletHome, PostApiV1AppletHomeResponse, postApiV1AppletProductList } from 'src/services/apis'
 import './index.less'
 import { creatQuery } from '../../services'
@@ -20,7 +19,6 @@ const useProductList = creatQuery<{ search: any }, any>({
 })
 
 const Index = () => {
-    const params = useParams()
     const [productListParams, setProductListParams] = useState({
         "search": "",
         "category_id": ""
@@ -33,38 +31,71 @@ const Index = () => {
         }
     })
 
-
     return (
-        <View>
-            <SearchInput onSearch={v => setProductListParams(old => ({ ...old, search: v }))} />
+        <Box background='#F3F3F3'>
+            <VStack spacing='8px'>
+                <Box margin='0 15rpx' >
+                    <SearchInput onSearch={v => setProductListParams(old => ({ ...old, search: v }))} />
+                </Box>
+                <Box height='180px'>
+                    <Swiper>
+                        {data?.data.top_banner.list.map(e => {
+                            return (
+                                <SwiperItem key={e.image}>
+                                    <Center>
+                                        <Image src={e.image!} mode='widthFix' style={{ width: '100%', height: '100%' }} />
+                                    </Center>
+                                </SwiperItem>
+                            )
+                        })}
+                    </Swiper>
+                </Box>
+            </VStack>
             <Tabs
                 onChange={i => {
                     const id = data?.data.category.list[i].category_id
                     setProductListParams(old => ({ ...old, category_id: id! }))
                 }}
+                activeStyle={{
+                    color: '#C08C4B'
+                }}
             >
-                <TabList>
-                    {data?.data.category.list.map(item => {
-                        return (
-                            <TabItem key={item.category_id}>{item.category_name}</TabItem>
-                        )
-                    })}
-                    <TabLine />
-                </TabList>
-                <TabPanels height='100vh'>
+                <Flex height='45px' background='#fff' alignItems='center'>
+                    <TabList
+                        spacing='30px'
+                        height='max-content'
+                    >
+                        {data?.data.category.list.map((item, index) => {
+                            return (
+                                <TabItem
+                                    padding={0}
+                                    marginLeft={index == 0 ? '15px' : ''}
+                                    fontSize='18px'
+                                    fontWeight='bold'
+                                    key={item.category_id}
+                                >
+                                    {item.category_name}
+                                    {data?.data.category.list.length > index + 1 && <Box background='#ccc' height='10px' width='1px' transform='translateX(15px)' />}
+                                </TabItem>
+                            )
+                        })}
+                        <TabLine style={{ background: '#C08C4B' }} />
+                    </TabList>
+                </Flex>
+                <TabPanels height='100vh' marginTop='10px'>
                     {
                         data?.data.category.list.map(item => (
                             <TabPanel key={item.category_id}>
-                                <Flex flexWrap='wrap' justifyContent='space-between' padding='0 12px'>
+                                <Flex flexWrap='wrap' justifyContent='space-between' padding='0 8px'>
                                     {productList?.data.list.map((e: any) => {
                                         return (
-                                            <Box key={e.product_id}>
+                                            <Box key={e.product_id} width='46vw' marginBottom='10px' background='#fff' borderRadius='5px'>
                                                 <VStack spacing='8px'>
-                                                    <Image src={e.cover} style={{ borderRadius: '8px', width: '45vw', height: '45vw' }} />
+                                                    <Image src={e.cover} style={{ borderRadius: '5px 5px 0 0', width: '100%', height: '45vw' }} />
                                                 </VStack>
-                                                <VStack spacing='4px'>
-                                                    <Flex>{e.product_name}</Flex>
-                                                    <Flex>¥{e.discount_price}</Flex>
+                                                <VStack spacing='5px' height='118px' padding='8px' paddingBottom={0}>
+                                                    <Flex noOfLines={1}>{e.product_name}</Flex>
+                                                    <Flex fontSize='18px' fontWeight='bold' color='#FE2811'>¥{e.discount_price}</Flex>
                                                 </VStack>
                                             </Box>
                                         )
@@ -75,7 +106,7 @@ const Index = () => {
                     }
                 </TabPanels>
             </Tabs>
-        </View>
+        </Box >
     )
 }
 export default Index
